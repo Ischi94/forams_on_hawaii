2526dde09f37f41d60da66e28b144d4ada1002fa
Ischi94 <gmathes94@gmx.de>
2021-04-11 13:30:29 +0200

    add model check plots

--- bayesian_model.R
+++ bayesian_model.R
@@ -1,6 +1,11 @@
 library(here)
 library(tidyverse)
 library(brms)
+library(bayesplot)
+
+
+# set-up ------------------------------------------------------------------
+
 
 # load raw data with foram index and distance to settlements
 dat_fi <- read_csv(here("data/raw_fi.csv"))
@@ -16,6 +21,10 @@ z_score <- function(x){
 dat_fi_std <- dat_fi %>% 
   mutate(across(fi:dist_mcbh, z_score)) %>% 
   select(-depth)
+
+
+# model fitting -----------------------------------------------------------
+
 
 # fit null model for comparison 
 m0 <- brm(
@@ -35,5 +44,58 @@ m1 <- brm(
             prior(cauchy(0, 1), class = sigma)), 
   iter = 2000, warmup = 500, chains = 4, 
   cores = 8, seed = 5, file = here("model/actual_model"))
+
+
+# model check -------------------------------------------------------------
+
+# effective number of samples
+
+neff_plot <- neff_ratio(m1) %>% 
+  enframe(name = "estimate", value = "neff_ratio") %>% 
+  mutate(estimate = c("Intercept", 
+                      "Distance Kan'eohe", 
+                      "Distance Kahalu'u", 
+                      "Distance MCBH", 
+                      "Sigma", 
+                      "LP"), 
+         estimate = as_factor(estimate), 
+         estimate = fct_reorder(estimate, neff_ratio)) %>% 
+  filter(estimate != "LP") %>% 
+  ggplot(aes(neff_ratio, estimate)) +
+  geom_vline(xintercept = 0.1, 
+             colour = "firebrick", alpha = 0.4, 
+             linetype = "dashed") +
+  geom_segment(aes(x = 0, xend = neff_ratio, 
+                   y = estimate, yend = estimate), 
+               colour = "steelblue", size = 0.8) +
+  geom_point(shape = 21, size = 3, 
+             fill = "steelblue", colour = "grey30", 
+             stroke = 1) +
+  labs(y = NULL, x = "Effective sample size ratio") +
+  coord_cartesian(xlim = c(0, 0.75)) +
+  theme_minimal()
+
+# rhat values
+rhat_plot <- rhat(m1) %>% 
+  enframe(name = "estimate", value = "rhat") %>% 
+  mutate(estimate = c("Intercept", 
+                      "Distance Kan'eohe", 
+                      "Distance Kahalu'u", 
+                      "Distance MCBH", 
+                      "Sigma", 
+                      "LP")) %>% 
+  ggplot(aes(rhat, estimate)) +
+  geom_point(shape = 21, size = 3, 
+             fill = "steelblue", colour = "grey30", 
+             stroke = 1) +
+  geom_vline(xintercept = 1.05,
+             colour = "firebrick", alpha = 0.4, 
+             linetype = "dashed") +
+  labs(y = NULL, x = "Rhat value") +
+  theme_minimal()
+
+# trace plot for convergence
+trank_plot <- mcmc_rank_overlay(m1) +
+  theme_minimal()
 
 
