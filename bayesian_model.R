c5a084f95da65848e1d42740ec5095243aecf00d
Ischi94 <gmathes94@gmx.de>
2021-04-11 14:11:30 +0200

    add distance depth model

--- bayesian_model.R
+++ bayesian_model.R
@@ -2,7 +2,7 @@ library(here)
 library(tidyverse)
 library(brms)
 library(bayesplot)
-
+library(ggridges)
 
 # set-up ------------------------------------------------------------------
 
@@ -19,8 +19,7 @@ z_score <- function(x){
 
 # standardize data for improve model fit and prior selection
 dat_fi_std <- dat_fi %>% 
-  mutate(across(fi:dist_mcbh, z_score)) %>% 
-  select(-depth)
+  mutate(across(depth:dist_mcbh, z_score))
 
 
 # model fitting -----------------------------------------------------------
@@ -35,7 +34,7 @@ m0 <- brm(
             iter = 2000, warmup = 500, chains = 4, 
             cores = 8, seed = 5, file = here("model/null_model"))
 
-# fit actual model
+# fit distance only model
 m1 <- brm(
   data = dat_fi_std, family = gaussian, 
   fi ~ 1 + dist_kan + dist_kah + dist_mcbh, 
@@ -43,7 +42,31 @@ m1 <- brm(
             prior(normal(0, 0.5), class = b),
             prior(cauchy(0, 1), class = sigma)), 
   iter = 2000, warmup = 500, chains = 4, 
-  cores = 8, seed = 5, file = here("model/actual_model"))
+  cores = 8, seed = 5, file = here("model/distance_model"))
+
+# fit distance and depth model
+m2 <- brm(
+  data = dat_fi_std, family = gaussian, 
+  fi ~ 1 + dist_kan + dist_kah + dist_mcbh + depth, 
+  prior = c(prior(normal(0, 0.2), class = Intercept),
+            prior(normal(0, 0.5), class = b),
+            prior(cauchy(0, 1), class = sigma)), 
+  iter = 2000, warmup = 500, chains = 4, 
+  cores = 8, seed = 5, file = here("model/distance_depth_model"))
+
+# model comparison --------------------------------------------------------
+
+# add LOO as criterion
+m0 <- add_criterion(m0, criterion = "loo")
+
+m1 <- add_criterion(m1, criterion = "loo")
+
+m2 <- add_criterion(m2, criterion = "loo")
+
+# compare models
+comp <- loo_compare(m0, m1, m2, criterion = "loo")
+
+print(comp, digits = 2, simplify = FALSE)
 
 
 # model check -------------------------------------------------------------
@@ -100,16 +123,53 @@ trank_plot <- mcmc_rank_overlay(m1) +
 
 
 
-# model comparison --------------------------------------------------------
-
-# add LOO as criterion
-m0 <- add_criterion(m0, "loo")
-
-m1 <- add_criterion(m1, "loo")
-
-# compare models
-comp <- loo_compare(m0, m1, criterion = "loo")
-
-print(comp, digits = 2, simplify = FALSE)
 
 
+# analyze results ---------------------------------------------------------
+
+ridge_plot <- posterior_samples(m1) %>% 
+  as_tibble() %>% 
+  select("Distance Kan'eohe" = b_dist_kan, 
+         "Distance Kahalu'u" = b_dist_kah, 
+         "Distance MCBH" = b_dist_mcbh) %>% 
+  pivot_longer(cols = everything(), 
+               names_to = "estimate") %>% 
+  ggplot(aes(value, estimate, fill = estimate)) +
+  stat_density_ridges(quantile_lines = TRUE, 
+                      quantiles = 2, 
+                      scale = 2.5, rel_min_height = 0.001, 
+                      colour = "white", alpha = 0.7) +
+  scale_fill_manual(values = c("steelblue", "firebrick", "darkgreen")) +
+  theme_minimal()
+
+posterior_samples(m1) %>% 
+  as_tibble() %>% 
+  select("Distance Kan'eohe" = b_dist_kan, 
+         "Distance Kahalu'u" = b_dist_kah, 
+         "Distance MCBH" = b_dist_mcbh) %>% 
+  pivot_longer(cols = everything(), 
+               names_to = "estimate") %>% 
+  group_by(estimate) %>% 
+  summarise(median_val = median(value), 
+            ci_outer = list(rethinking::PI(value, prob = 0.95)), 
+            ci_inner = list(rethinking::PI(value, prob = 0.89))) %>% 
+  mutate(ci_outer_low = map_dbl(ci_outer, pluck(1)), 
+         ci_outer_high = map_dbl(ci_outer, pluck(2)), 
+         ci_inner_low = map_dbl(ci_inner, pluck(1)), 
+         ci_inner_high = map_dbl(ci_inner, pluck(2))) %>% 
+  ggplot(aes(y = estimate, yend = estimate)) +
+  geom_vline(xintercept = 0, 
+             linetype = "dashed", 
+             colour = "grey10") +
+  geom_segment(aes(x = ci_inner_low, xend = ci_inner_high), 
+               size = 2.3, colour = "firebrick", 
+               alpha = 0.8) +
+  geom_segment(aes(x = ci_outer_low, xend = ci_outer_high), 
+               size = 0.8, colour = "firebrick", 
+               alpha = 0.8) +
+  geom_point(aes(x = median_val), 
+             shape = 21, size = 5, 
+             fill = "steelblue", colour = "grey30", 
+             stroke = 1) +
+  labs(y = NULL, x = "Coefficient estimate") +
+  theme_minimal()
